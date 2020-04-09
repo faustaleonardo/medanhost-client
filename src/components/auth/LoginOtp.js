a58983b4ce0a7d8aa83d62c2e90eba893a491201
Fausta Leonardo <faustaleonardo11@gmail.com>
2020-04-09 13:39:05 +0700

    added verify otp modal

--- src/components/auth/LoginOtp.js
+++ src/components/auth/LoginOtp.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import { Button, Form } from 'semantic-ui-react';
+import React, { useState } from 'react';
+import { Button, Form, Modal } from 'semantic-ui-react';
 
 export default () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <div className="general-form center-vh">
       <Form>
         <Form.Input label="Email" placeholder="johndoe@lorem.com" />
-        <Button positive>Submit</Button>
+        <Button positive onClick={handleOpen}>
+          Submit
+        </Button>
       </Form>
+
+      <Modal size={'mini'} open={open} onClose={() => setOpen(false)}>
+        <Modal.Header>Type your OTP Code</Modal.Header>
+        <Modal.Content>
+          <Form>
+            <Form.Input label="OTP Code" placeholder="000000" />
+          </Form>
+        </Modal.Content>
+        <Modal.Actions>
+          <Button positive onClick={handleClose}>
+            Submit
+          </Button>
+        </Modal.Actions>
+      </Modal>
     </div>
   );
 };
--- src/components/partials/Navigation.js
+++ src/components/partials/Navigation.js
@@ -1,15 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Menu } from 'semantic-ui-react';
 import LoginModal from 'components/auth/LoginModal';
 import { useHistory } from 'react-router-dom';
 
 export default () => {
-  const [activeItem, setActiveItem] = useState('home');
   const history = useHistory();
 
   const handleItemClick = (e, { name }) => {
-    setActiveItem(name);
-
     switch (name) {
       case 'home':
         history.push('/');
@@ -25,16 +22,8 @@ export default () => {
   return (
     <Menu borderless>
       <Menu.Item name="Medanhost" />
-      <Menu.Item
-        name="home"
-        active={activeItem === 'home'}
-        onClick={handleItemClick}
-      />
-      <Menu.Item
-        name="rooms"
-        active={activeItem === 'rooms'}
-        onClick={handleItemClick}
-      />
+      <Menu.Item name="home" onClick={handleItemClick} />
+      <Menu.Item name="rooms" onClick={handleItemClick} />
       <Menu.Menu position="right">
         <Menu.Item>
           <LoginModal />
