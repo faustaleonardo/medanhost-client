cbffa335cb3227cced34c8a3d9fc1dbfc2a08a7f
Fausta Leonardo <faustaleonardo11@gmail.com>
2020-04-12 21:26:54 +0700

    update map

--- src/components/host/modals/LocationModal.js
+++ src/components/host/modals/LocationModal.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Modal, Form, Button } from 'semantic-ui-react';
+import * as opencage from 'opencage-api-client';
 
 export default ({ open, setOpen }) => {
-  const searchLocation = () => {};
+  const [query, setQuery] = useState('');
+
+  const searchLocation = async () => {
+    try {
+      const response = await opencage.geocode({
+        key: process.env.REACT_APP_OPENCAGE_API_KEY,
+        q: query,
+      });
+      console.log(response);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <Modal size={'small'} open={open} onClose={() => setOpen(false)}>
@@ -11,7 +24,12 @@ export default ({ open, setOpen }) => {
         <p>My location is at ...</p>
         {/* form */}
         <Form onSubmit={searchLocation}>
-          <Form.Input fluid placeholder="Location" />
+          <Form.Input
+            fluid
+            placeholder="Location"
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+          />
         </Form>
 
         {/* result */}
