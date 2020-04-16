da469149bae7da003a02d4a361b6594c7f804534
Fausta Leonardo <faustaleonardo11@gmail.com>
2020-04-16 21:49:54 +0700

    update url

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
+const baseUrlDev = 'http://localhost:5000/';
+const baseUrlProd = 'https://ce16ca29.ngrok.io';
+
 const jwt = localStorage.getItem('jwt');
 export default axios.create({
-  baseURL: 'http://localhost:5000/',
+  baseURL: baseUrlProd,
   headers: {
     common: {
       Authorization: `Bearer ${jwt}`,
